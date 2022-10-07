a1e4b7d123132dfcaa50f2b08ea72bf76340575a
Gábor Jambrik <gabor.jambrik@stud.uni-corvinus.hu>
2022-10-07 12:49:58 +0200

    Excel WB creation and opening

--- Excel/Excel/Form1.cs
+++ Excel/Excel/Form1.cs
@@ -7,22 +7,58 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel2 = Microsoft.Office.Interop.Excel;
+using System.Reflection;
 
 namespace Excel
 {
+    
     public partial class Form1 : Form
     {
-        RealEstateEntities context = new RealEstateEntities();
+        RealEstateEntities1 context = new RealEstateEntities1();
         List<Flat> Flats;
-         
+        Excel2.Application xlApp;
+        Excel2.Workbook xlWB;
+        Excel2.Worksheet xlSheet;
+
+
         public Form1()
         {
             InitializeComponent();
             LoadData();
+            CreateExcel();       
         }
-        void LoadData()
+
+        private void LoadData()
         {
             Flats = context.Flats.ToList();
         }
+
+        private void CreateExcel()
+        {
+            try
+            {
+                xlApp = new Excel2.Application();
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+                xlSheet = xlWB.ActiveSheet;
+                //CreateTable();
+                xlApp.Visible = true;
+                xlWB.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+
+
+            }
+            
+        }
+
+        
     }
 }
