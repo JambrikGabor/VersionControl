96a2e381d59d69fde938e0f6424748ec2b1500bd
Gábor Jambrik <gabor.jambrik@stud.uni-corvinus.hu>
2022-11-04 13:00:56 +0100

    Before Car Image

---
+++ ToyFactory/ToyFactory/Entities/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToyFactory.Abstractions;
+
+namespace ToyFactory.Entities
+{
+    public class Car : Toy
+    {
+        protected override void DrawImage(Graphics g)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
