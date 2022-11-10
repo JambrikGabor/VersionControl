28ebd22eb981b5c2d94604d79c3606635a27ebba
Parper <halal.gyilkos@gmail.com>
2022-11-10 17:18:20 +0100

    Database Connection + ORM + Data Loading + CreatePortfolio

--- VARgyak07/VARgyak07/Form1.cs
+++ VARgyak07/VARgyak07/Form1.cs
@@ -7,14 +7,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using VARgyak07.Entities;
 
 namespace VARgyak07
 {
     public partial class Form1 : Form
     {
+        PortfolioEntities context = new PortfolioEntities();
+        List<Tick> Ticks;
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
         public Form1()
         {
             InitializeComponent();
+            Ticks = context.Ticks.ToList();
+            dataGridView1.DataSource = Ticks;
+            CreatePortfolio();
+        }
+
+        private void CreatePortfolio()
+        {
+            Portfolio.Add(new PortfolioItem { Index = "OTP", Volume = 10 });
+            Portfolio.Add(new PortfolioItem { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new PortfolioItem { Index = "ELMU", Volume = 10 });
+            //Portfolio.Add(new PortfolioItem("OTP", 10));
+            //Portfolio.Add(new PortfolioItem("ZWACK", 10));
+            //Portfolio.Add(new PortfolioItem("ELMU", 10));
+            
+            dataGridView2.DataSource = Portfolio;
         }
     }
 }
