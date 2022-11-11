80a43190275e5c5804d9a2e947fdb1c7831c8622
Gábor Jambrik <gabor.jambrik@stud.uni-corvinus.hu>
2022-11-11 12:34:18 +0100

    Product

---
+++ Restaurant/Restaurant/Abstractions/Product.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Restaurant.Enitities
+{
+    public abstract class Product : Button
+    {
+        public Product()
+        {
+            Width = 150;
+            Height = 50;
+        }
+        public string _foodname;
+        public string Title 
+        { 
+            get { return _foodname; } 
+            set 
+            {
+                _foodname = value;
+                Text = Title; } 
+        }
+        private int calorynumber;
+        public int Calories 
+        {
+            get 
+            {
+                return calorynumber;
+            } 
+            set 
+            {
+                calorynumber = value;
+                Display();
+            } 
+        }
+        protected abstract void Display();
+    }
+}
