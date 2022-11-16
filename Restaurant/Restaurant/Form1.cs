0ac3bf57534ff0ea85cbd9686edff5caae369de1
Parper <halal.gyilkos@gmail.com>
2022-11-16 12:05:09 +0100

    3.2

--- Restaurant/Restaurant/Form1.cs
+++ Restaurant/Restaurant/Form1.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Restaurant.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,10 +14,23 @@ namespace Restaurant
 {
     public partial class Form1 : Form
     {
+        List<Product> _products = new List<Product>();
         public Form1()
         {
             InitializeComponent();
+            XMLFunction("");
 
+        }
+
+        private string XMLFunction(string filename)
+        {
+            using (var sr = new StreamReader(filename,Encoding.Default))
+            {
+                var xml = sr.ReadToEnd();
+                return xml;
+            }
+            
+            
         }
     }
 }
