e3d2ebd1731e6e71ed7514e5821ce0dfa1f8ddc8
Parper <halal.gyilkos@gmail.com>
2022-11-17 18:09:24 +0100

    Finished

--- Restaurant/Restaurant/Form1.cs
+++ Restaurant/Restaurant/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using Restaurant.Abstractions;
+using Restaurant.Entities;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -6,6 +7,7 @@ using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -20,14 +22,56 @@ namespace Restaurant
         {
             InitializeComponent();
             LoadXML();
-            
+            ShowProducts();
 
+        }
+
+        private void ShowProducts()
+        {
+            //_products = (from x in _products orderby x.Title select x).ToList();
+            _products = _products.OrderBy(x => x.Title).ToList();
+
+            for (int i = 0; i < _products.Count; i++)
+            {
+                _products[i].Location = new Point(5, 50 * i + 10);
+                this.Controls.Add(_products[i]);
+            }
         }
 
         private void LoadXML()
         {
             var xml = new XmlDocument();
-            xml.LoadXml(XMLFunction("Menu.xml"));
+            string prePath = Directory.GetParent(Environment.CurrentDirectory).Parent.FullName;
+            string path = Path.Combine(prePath, "Menu.xml");
+            xml.LoadXml(XMLFunction(path));
+
+            XmlNodeList nl = xml.DocumentElement.ChildNodes;
+            foreach (XmlElement node in nl)
+            {
+                string type = node.SelectSingleNode("type").InnerText;
+                string name = node.SelectSingleNode("name").InnerText;
+                string description = node.SelectSingleNode("description").InnerText;
+                string calories = node.SelectSingleNode("calories").InnerText;
+                
+                if (type == "food")
+                {
+                    Food f = new Food();
+                    f.Title = name;
+                    f.Calories = int.Parse(calories);
+                    f.Description = description;
+
+                    _products.Add(f);
+                }
+                else
+                {
+                    Drink d = new Drink();
+                    d.Title = name;
+                    d.Calories = int.Parse(calories);
+
+                    _products.Add(d);
+                }
+
+            }
         }
 
         private string XMLFunction(string filename)
