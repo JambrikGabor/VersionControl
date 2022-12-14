b72084291ecccd195d38b13531a6aad694b6982e
Parper <halal.gyilkos@gmail.com>
2022-12-14 11:24:33 +0100

    2.1 & 2.2

--- ZC3A7LMintaZH3_2/ZC3A7LMintaZH3_2/Form1.Designer.cs
+++ ZC3A7LMintaZH3_2/ZC3A7LMintaZH3_2/Form1.Designer.cs
@@ -29,13 +29,118 @@ namespace ZC3A7LMintaZH3_2
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxBehaviour = new System.Windows.Forms.TextBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Név";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Viselkedés";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(103, 23);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(100, 22);
+            this.textBoxName.TabIndex = 2;
+            // 
+            // textBoxBehaviour
+            // 
+            this.textBoxBehaviour.Location = new System.Drawing.Point(103, 59);
+            this.textBoxBehaviour.Name = "textBoxBehaviour";
+            this.textBoxBehaviour.Size = new System.Drawing.Size(100, 22);
+            this.textBoxBehaviour.TabIndex = 3;
+            this.textBoxBehaviour.Text = "3";
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(47, 124);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(134, 60);
+            this.buttonAdd.TabIndex = 4;
+            this.buttonAdd.Text = "Hozzáad";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(255, 23);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(511, 285);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(325, 367);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(131, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "A rosszak száma: 0";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(598, 340);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 71);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.textBoxBehaviour);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxBehaviour;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- ZC3A7LMintaZH3_2/ZC3A7LMintaZH3_2/Form1.cs
+++ ZC3A7LMintaZH3_2/ZC3A7LMintaZH3_2/Form1.cs
@@ -7,14 +7,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ZC3A7LMintaZH3_2.Entities2;
 
 namespace ZC3A7LMintaZH3_2
 {
     public partial class Form1 : Form
     {
+        BindingList<Child> children = new BindingList<Child>();
         public Form1()
         {
             InitializeComponent();
+            dataGridView1.DataSource = children;
         }
     }
 }
